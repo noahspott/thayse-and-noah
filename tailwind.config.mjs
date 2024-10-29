06d8cefeca755e972a19ebdb86f19465dc222dfd
noahspott <noahspottdev@gmail.com>
2024-10-29 00:56:28 -0400

    create more info page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {		
-			colors: {
-			'thay-green': '#9fbaa8',
-		},
-	},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "thay-green": {
+          50: "#f6f7f5",
+          100: "#ecefec",
+          200: "#d9ded8",
+          300: "#c6cec5",
+          400: "#b3bdb1",
+          500: "#a0ad9e",
+          600: "#808a7e",
+          700: "#60685f",
+          800: "#40453f",
+          900: "#202320",
+          950: "#101110",
+        },
+        dark: {
+          1: "#2e2e2e",
+          2: "#3a3a3a",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
