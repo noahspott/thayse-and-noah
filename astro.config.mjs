6bbab429d3c062d20fc3e44f645bf47dc91e2318
noahspott <noahspottdev@gmail.com>
2024-11-22 02:30:34 -0500

    - add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: netlify(),
 });
